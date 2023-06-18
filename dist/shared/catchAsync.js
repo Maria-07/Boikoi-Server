0c94d0dd873a95aae60ec32db69fa2e05f8f4e44
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-06-18 12:53:59 +0600

    User ApI end point added and tested

--- dist/shared/catchAsync.js
+++ dist/shared/catchAsync.js
@@ -1,22 +1,46 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsync = (fn) => {
-    return (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+      function fulfilled(value) {
         try {
-            yield fn(req, res, next);
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
         }
-        catch (error) {
-            next(error);
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
         }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const catchAsync = fn => {
+  return (req, res, next) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+      try {
+        yield fn(req, res, next);
+      } catch (error) {
+        next(error);
+      }
     });
 };
 exports.default = catchAsync;
