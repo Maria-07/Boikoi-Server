35210dbf157310b9ecd7a6e3ba062fb14ad237d4
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-06-25 14:10:36 +0600

    [Done] cow Update and Delete Can only be accessed by the seller of the cow

--- dist/app/modules/auth/auth.route.js
+++ dist/app/modules/auth/auth.route.js
@@ -1,17 +1,39 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.AuthRoutes = void 0;
-const express_1 = __importDefault(require("express"));
-const user_controller_1 = require("../user/user.controller");
-const validateRequest_1 = __importDefault(require("../../middlewares/validateRequest"));
-const user_validation_1 = require("../user/user.validation");
-const auth_controller_1 = require("./auth.controller");
-const auth_validation_1 = require("./auth.validation");
+const express_1 = __importDefault(require('express'));
+const user_controller_1 = require('../user/user.controller');
+const validateRequest_1 = __importDefault(
+  require('../../middlewares/validateRequest')
+);
+const user_validation_1 = require('../user/user.validation');
+const auth_controller_1 = require('./auth.controller');
+const auth_validation_1 = require('./auth.validation');
 const router = express_1.default.Router();
-router.post('/sign-up', (0, validateRequest_1.default)(user_validation_1.UserValidation.createUserZodSchema), user_controller_1.UserController.createUser);
-router.post('/login', (0, validateRequest_1.default)(auth_validation_1.AuthValidation.loginZodSchema), auth_controller_1.AuthController.loginUser);
-router.post('/refresh-token', (0, validateRequest_1.default)(auth_validation_1.AuthValidation.refreshTokenZodSchema), auth_controller_1.AuthController.refreshToken);
+router.post(
+  '/sign-up',
+  (0, validateRequest_1.default)(
+    user_validation_1.UserValidation.createUserZodSchema
+  ),
+  user_controller_1.UserController.createUser
+);
+router.post(
+  '/login',
+  (0, validateRequest_1.default)(
+    auth_validation_1.AuthValidation.loginZodSchema
+  ),
+  auth_controller_1.AuthController.loginUser
+);
+router.post(
+  '/refresh-token',
+  (0, validateRequest_1.default)(
+    auth_validation_1.AuthValidation.refreshTokenZodSchema
+  ),
+  auth_controller_1.AuthController.refreshToken
+);
 exports.AuthRoutes = router;
--- dist/app/modules/user/user.model.js
+++ dist/app/modules/user/user.model.js
@@ -1,59 +1,94 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.User = void 0;
 /* eslint-disable @typescript-eslint/no-this-alias */
-const mongoose_1 = require("mongoose");
-const user_constance_1 = require("./user.constance");
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const config_1 = __importDefault(require("../../../config"));
-const UserSchema = new mongoose_1.Schema({
+const mongoose_1 = require('mongoose');
+const user_constance_1 = require('./user.constance');
+const bcrypt_1 = __importDefault(require('bcrypt'));
+const config_1 = __importDefault(require('../../../config'));
+const UserSchema = new mongoose_1.Schema(
+  {
     phoneNumber: { type: String, required: true, unique: true },
     role: { type: String, enum: user_constance_1.role, required: true },
     password: { type: String, required: true, select: 0 },
     name: {
-        required: true,
-        type: {
-            firstName: { type: String, required: true },
-            lastName: { type: String, required: true },
-        },
+      required: true,
+      type: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+      },
     },
     address: { type: String, required: true },
     budget: { type: Number, required: true },
     income: { type: Number, required: true },
-}, {
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
+      virtuals: true,
     },
-});
+  }
+);
 UserSchema.statics.isUserExist = function (phoneNumber) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield exports.User.findOne({ phoneNumber }, { phoneNumber: 1, role: 1, password: 1 });
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    return yield exports.User.findOne(
+      { phoneNumber },
+      { phoneNumber: 1, role: 1, password: 1 }
+    );
+  });
 };
 UserSchema.statics.isPasswordMatch = function (givenPassword, savedPassword) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield bcrypt_1.default.compare(givenPassword, savedPassword);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    return yield bcrypt_1.default.compare(givenPassword, savedPassword);
+  });
 };
 UserSchema.pre('save', function (next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const user = this;
-        //hash password
-        user.password = yield bcrypt_1.default.hash(user.password, Number(config_1.default.bcrypt_salt_rounds));
-        next();
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    const user = this;
+    //hash password
+    user.password = yield bcrypt_1.default.hash(
+      user.password,
+      Number(config_1.default.bcrypt_salt_rounds)
+    );
+    next();
+  });
 });
 exports.User = (0, mongoose_1.model)('User', UserSchema);
